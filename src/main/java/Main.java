371ec00db7160c6580b4329a9bdae5dac0ae2351
Olga <olakostina31@gmail.com>
2023-01-11 22:33:32 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,17 @@ public class Main {
 
         long amount = 1000_60;
         boolean registered = true;
+        long expected = 30;
 
-        service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
+        if (expected == actual) {
+            System.out.println("passed");
+        } else {
+            System.out.println("failed");
+        }
     }
 }
+
+
+
 
